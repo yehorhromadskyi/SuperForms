098e6cb1dc78c2f53a3fb4c6d1d55bc05f2ef4b9
EXOFT-USER <EXOFT-USER@DESKTOP-NVTEKIO>
2016-07-20 18:31:43 +0300

    Added TaskExtensions

---
+++ SuperForms/Shared/SuperForms.Core/Extensions/TaskExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SuperForms.Core.Extensions
+{
+    public static class TaskExtensions
+    {
+        public static IEnumerable<Task<T>> InCompletionOrder<T>(this IEnumerable<Task<T>> source)
+        {
+            var inputs = source.ToList();
+            var boxes = inputs.Select(x => new TaskCompletionSource<T>()).ToList();
+
+            var currentIndex = -1;
+            foreach (var task in inputs)
+            {
+                task.ContinueWith(completed =>
+                {
+                    var nextBox = boxes[Interlocked.Increment(ref currentIndex)];
+                    PropagateResult(completed, nextBox);
+                }, TaskContinuationOptions.ExecuteSynchronously);
+            }
+
+            return boxes.Select(box => box.Task);
+        }
+
+        private static void PropagateResult<T>(Task<T> completedTask,
+            TaskCompletionSource<T> completionSource)
+        {
+            switch (completedTask.Status)
+            {
+                case TaskStatus.RanToCompletion:
+                    completionSource.TrySetResult(completedTask.Result);
+                    break;
+                case TaskStatus.Canceled:
+                    completionSource.TrySetCanceled();
+                    break;
+                case TaskStatus.Faulted:
+                    completionSource.TrySetException(completedTask.Exception);
+                    break;
+                default:
+                    throw new ArgumentException("Task was not completed");
+            }
+        }
+    }
+}
